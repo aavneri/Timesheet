e77949432c56613e236696b58f85cd8e993c20c9
aavneri <avneri.ari@gmail.com>
2023-09-06 11:04:11 -0700

    adding bootstrap, header, footer and home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
-  return (
-    <div>
-      my App
-    </div>
-  );
+    return (
+        <Router>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <Routes>
+                        <Route path="/" element={<HomeScreen />} exact />
+                    </Routes>
+                </Container>
+            </main>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+function HomeScreen() {
+  return (
+      <Container className=''>
+          <h1>Welcome to TimeSheet</h1>
+      </Container>
+  );
+}
+
+export default HomeScreen
