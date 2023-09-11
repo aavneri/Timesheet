5297b588147dd5a23634cb406a2c66da75c0472d
aavneri <avneri.ari@gmail.com>
2023-09-10 20:32:19 -0700

    adding all missing static files

--- backend/settings.py
+++ backend/settings.py
@@ -99,7 +99,6 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
             os.path.join(BASE_DIR, "frontend/build"),
-            os.path.join(BASE_DIR, "templates"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
