b829a9fd8885a28883661e160394a243472495d7
aavneri <avneri.ari@gmail.com>
2023-09-10 20:16:15 -0700

    adding templates folder

--- backend/settings.py
+++ backend/settings.py
@@ -97,7 +97,10 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "frontend/build")],
+        "DIRS": [
+            os.path.join(BASE_DIR, "frontend/build"),
+            os.path.join(BASE_DIR, "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
